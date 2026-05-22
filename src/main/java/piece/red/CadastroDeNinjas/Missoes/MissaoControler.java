b4ddc2032b052db967605a7ac40b093f12ab245f
Yukkinho <alvarohenriquee73@gmail.com>
2026-05-21 21:25:37 -0300

    feat: Atualiza o MissaoControler com Mock de dados

--- src/main/java/piece/red/CadastroDeNinjas/Missoes/MissaoControler.java
+++ src/main/java/piece/red/CadastroDeNinjas/Missoes/MissaoControler.java
@@ -1,17 +1,23 @@
 package piece.red.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissaoControler {
 
-    @GetMapping("/missoes")
-    public String boasVindas() {
+   @GetMapping("/listar")
+    public String listarMissao() {
+       return "Missao listada com sucesso";
+   }
 
-        return "Bem vindo, datebayo!!.";
+   @PostMapping("/criar")
+    public String criarMissao() {
+       return "Missao criada com sucesso";
+   }
 
-}
+   @PutMapping("/alternar")
+    public String alternarMissao(){
+       return "Missao alternada com sucesso";
+   }
 }
